1286af7aa22c1778c41178adc57de0fc96e9ddb2
mensah attisso <attmensah@gmail.com>
2026-03-18 21:48:31 -0400

    20260317-200

--- frontend/src/app/layout/nav-bar/nav-bar.component.ts
+++ frontend/src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,13 +1,10 @@
 import { Component, ChangeDetectionStrategy, input, output } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [MatIconModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
--- frontend/src/app/layout/side-nav/side-nav.component.ts
+++ frontend/src/app/layout/side-nav/side-nav.component.ts
@@ -1,13 +1,12 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MenuItem } from '../../core/navigation.service';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatListModule, MatIconModule],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss',
